2a898dead26a6b0f1457130cdd7ace0356cfdfe8
Danush <sdanush182@gmail.com>
2020-08-16 22:13:15 +0530

    Delete delete.py

--- delete.py
+++
@@ -1,7 +0,0 @@
-import cx_Oracle
-conn=cx_Oracle.connect('SYSTEM/oracle@localhost')
-print(conn)
-cur=conn.cursor()
-statement = 'delete from table1 where age<19'
-cur.execute(statement)
-conn.commit()
