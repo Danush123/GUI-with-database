913580dee0cc2a364e21a9f4cdc3ce56cc9f2f29
Danush <sdanush182@gmail.com>
2020-08-16 22:13:27 +0530

    Delete insert.py

--- insert.py
+++
@@ -1,7 +0,0 @@
-import cx_Oracle
-conn=cx_Oracle.connect('SYSTEM/oracle@localhost')
-print(conn)
-cur=conn.cursor()
-statement = 'insert into table1(name, age) values (:1, :2)'
-cur.execute(statement, ('Sand', 3,))
-conn.commit()
